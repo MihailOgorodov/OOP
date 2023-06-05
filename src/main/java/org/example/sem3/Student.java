fab6c22b79ff69d81a172d815afbab58a6866999
“Mihail <mic311089@gmail.com>
2023-06-05 21:05:13 +0400

    sem4 added

--- src/main/java/org/example/sem3/Student.java
+++ src/main/java/org/example/sem3/Student.java
@@ -1,13 +1,12 @@
 package org.example.sem3;
 
-import lombok.*;
+
 
 //@Getter
 //@AllArgsConstructor
 //@Setter
 //@ToString
 //@EqualsAndHashCode
-@Data
 public class Student implements Comparable<Student> {
 
     private final Long id;
